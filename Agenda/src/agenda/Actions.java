fb6aa692fcfc84aec1e69deab5c6252eff3602b7
Humanoide-PC\Humanoide <luishumanoide>
2017-05-24 18:35:09 -0700

    cambios

---
+++ Agenda/src/agenda/Actions.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Humanoide
+ */
+public class Actions {
+    /**
+     * crear la cita con los siquientes parametros
+     * @param name , el sujeto con el que se irá a la cita, sustantivo
+     * @param start , inicio de la cita
+     * @param end , fin de la cita
+     * @return si se creó la cita o no
+     */
+    public static boolean createDate(String name, Date start, Date end){
+        //code
+        System.out.println(name+" "+start+" "+end);
+        return true;
+    }
+    
+    
+    /**
+     * eliminar la cita con el nombre del sustantuivo y la fecha dada
+     * Si la fecha está entre el tiempo de inicio de la cita y el tiempo de fin, entonces se elimina
+     * @param name
+     * @param start
+     * @return 
+     */
+    public static boolean deleteDate(String name, Date start){
+        //code
+        return true;
+    }
+    
+    
+    /**
+     * actualizo la cita, busco primero por el nombre name, y el tiempo start1, y la cambió al nuevo empiezo:
+     * start2 y al nuevo fin : end2
+     * @param name
+     * @param start1 el tiempo en el que está la cita anterior, o cuando empieza
+     * @param start2 el inicio nuevo
+     * @param end2 el fin nuevo
+     * @return 
+     */
+    public static boolean updateDate(String name, Date start1, Date start2, Date end2){
+        //code
+        return true;
+    }
+    
+}
---
+++ Agenda/src/translator/DateUtils.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package translator;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Humanoide
+ */
+public class DateUtils {
+
+    public static void main(String[] args) {
+       getDate(10,2,1998);
+    }
+
+    public Date getDate() {
+        Calendar cumpleCal = Calendar.getInstance();
+        cumpleCal.set(2010, 9, 27); //La hora no me interesa y recuerda que los meses van de 0 a 11
+        cumpleCal.add(Calendar.DAY_OF_YEAR, 7);
+        System.out.println(cumpleCal.getTime());
+        int dia = cumpleCal.get(Calendar.DAY_OF_WEEK);
+        System.out.println(dia); //Día 4 = WEDNESDAY = MIÉRCOLES
+        return null;
+    }
+    
+    public static Date getToday(){
+        
+    }
+
+    /**
+     * obtiene la fecha del día mas proximo dependiendo de la entrada
+     *
+     * @param day puede ser Monday, Tuesday....Sunday
+     * @return
+     */
+    public static Date getProxDay(String day) {
+        //encuentro el numero de dia con respecto al nombre
+        String days[] = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
+        int nDay = -1;
+        for (int i = 0; i < days.length; i++) {
+            if (days[i].contains(day)) {
+                nDay = i + 1;
+                break;
+            }
+        }
+        Calendar calendar = Calendar.getInstance();
+        for (int i = 0; i <= 7; i++) {
+            if (calendar.get(Calendar.DAY_OF_WEEK) == nDay) {
+                calendar.add(Calendar.DAY_OF_YEAR, 1);
+                break;
+            }
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+        }
+        //seteo la hora a una hora 0
+        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        return calendar.getTime();
+    }
+
+    /**
+     * obtiene una fecha con la hora, la fecha es la mínima del sistema
+     *
+     * @param hour
+     * @param meridian
+     * @return
+     */
+    public static Date getHour(String hour, String meridian) {
+        String[] hourParts = hour.split(":");
+        int hours = Integer.parseInt(hourParts[0]);
+        int minutes = Integer.parseInt(hourParts[1]);
+        if (meridian.equals("pm")) {
+            hours = hours + 12;
+        }
+        Calendar calendar = new GregorianCalendar();
+        calendar.clear();
+        calendar.set(Calendar.HOUR, hours);
+        calendar.set(Calendar.MINUTE, minutes);
+        System.out.println(calendar.getTime());
+        return calendar.getTime();
+    }
+
+    /**
+     * obtiene la hora si le doy una variable entera
+     *
+     * @param hour
+     * @param meridian
+     * @return
+     */
+    public static Date getHour(int hour, String meridian) {
+        if (meridian.equals("pm")) {
+            hour = hour + 12;
+        }
+        Calendar calendar = new GregorianCalendar();
+        calendar.clear();
+        calendar.set(Calendar.HOUR, hour);
+        System.out.println(calendar.getTime());
+        return calendar.getTime();
+    }
+    /**
+     * regresa la fecha con solo poner el dia
+     * @param day
+     * @return 
+     */
+    public static Date getDate(int day){
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        System.out.println(calendar.getTime());
+        return calendar.getTime();
+    }
+    /**
+     * regresa la fecha con solo el día y el mes
+     * @param day
+     * @param month
+     * @return 
+     */
+    public static Date getDate(int day, int month){
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.MONTH,month-1);
+        System.out.println(calendar.getTime());
+        return calendar.getTime();
+    }
+    /**
+     * regresa la fecha a partir del dia, mes y año
+     * @param day
+     * @param month
+     * @return 
+     */
+    public static Date getDate(int day, int month, int year){
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.MONTH,month-1);
+        calendar.set(Calendar.YEAR,year);
+        System.out.println(calendar.getTime());
+        return calendar.getTime();
+    }
+    
+
+}
