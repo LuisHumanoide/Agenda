b0446e95c64e227582013b1fe5228e189657f63d
Humanoide-PC\Humanoide <luishumanoide>
2017-06-07 06:34:20 -0700

    vergulas

---
+++ Agenda/src/agenda/WindowAgent.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+import jade.core.Agent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Humanoide
+ */
+public class WindowAgent extends Agent implements MouseListener{
+    
+    @Override
+    public void setup(){
+        JOptionPane.showMessageDialog(null, "holsdfa");
+        Intro in=new Intro();
+        in.getExecute().addMouseListener(this);
+        in.setVisible(true);
+    }
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        JOptionPane.showMessageDialog(null, "hola");
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
